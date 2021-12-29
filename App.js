fadcad2794d9389b8b50d75e39c311a91eb28d98
nikkumbh <prabalnikkumbh123@gmail.com>
2021-12-29 16:59:04 +0530

    added all the files

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList} from "react-native";
+import React , {useState} from "react";
 
 export default function App() {
+  
+  const [amount, setAmount] = useState();
+  const [Desc, setDesc] = useState("");
+  const [storeExpense, setStoreExpense] = useState([]);
+  const [storeIncome, setStoreIncome] = useState([]);
+
+
+  const handleAmount = (inputedAmount) => {
+    setAmount(inputedAmount);
+  }
+
+  const handleDesc = (inputedDesc) => {
+    setDesc(inputedDesc);
+  }
+
+  const handleExpenseClick = () => {
+    console.log("Expense is clicked");
+    setStoreExpense((currentState) => [...currentState , {amount , Desc}]);
+  }
+
+  const handleIncomeClick = () => {
+    console.log("Income is clicked");
+    setStoreIncome((currentState) => [...currentState , {amount, Desc}]);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Expense Tracker</Text>
+      <TextInput placeholder="Enter Amount"  onChangeText={handleAmount} value={amount} />
+      <TextInput placeholder="Enter Description" onChangeText={handleDesc} value={Desc}/>
+      <Button title="Expense" onPress={handleExpenseClick} />
+      <Button title="Income" onPress={handleIncomeClick} />
+      <Text>expense</Text>
+      <FlatList 
+        keyExtractor={(item, index)=> index}
+        data={storeExpense}
+        renderItem={(itemData) => (
+          <Text>
+            {itemData.item.amount}
+            {itemData.item.Desc}
+          </Text>
+        )}
+      /> 
+
+      <Text>income</Text>
+      <FlatList 
+        keyExtractor={(item, index)=> index}
+        data={storeIncome}
+        renderItem={(itemData) => (
+          <Text>
+            {itemData.item.amount}
+            {itemData.item.Desc}
+          </Text>
+        )}
+      /> 
+      
+      
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
